cc5f3cbdeb57df091d149b441368a37f16e1b20c
Ziqiang Huang <hzq.fox@gmail.com>
2025-10-10 11:33:23 +0200

    update data dimension check

--- code/MINFLUX data plot app/functions/check3D.m
+++ code/MINFLUX data plot app/functions/check3D.m
@@ -3,9 +3,13 @@ function check3D (app)
     if (isempty(app.data.loc_x))
         return;
     end
-    if (all(app.data.loc_z==0))
+    
+    if iqr(app.data.loc_z, 'all') < 1e-9    % if the 25%-75% interquantile range of Z axis value is less than 0.1 nm, consider the data as 2D
         app.nDim = 2;
+        % maybe re-zero all z values?
+        % app.data.loc_z = zeros( size(app.data.loc_z) );
     else
         app.nDim = 3;
     end
+
 end
