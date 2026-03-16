5018cc842acc1163eb9da3aff9eb50599b65145e
Ziqiang Huang <ziqiang.huang@embl.de>
2026-03-16 14:59:35 +0100

    Edit computeLocPrecision.m account for unit error and close to zero values

--- code/MINFLUX data plot app/functions/computeLocPrecision.m
+++ code/MINFLUX data plot app/functions/computeLocPrecision.m
@@ -2,7 +2,7 @@
             if isempty(app.data) || isempty(app.xyz)
                 return;
             end
-            minLoc = 2;
+            minLoc = 5;
             loc = app.xyz(app.ftr, :);      % get xyz coordinates after filter
             tid = app.data.tid(app.ftr);    % get tid array after filter
             uid = unique(tid);
@@ -39,7 +39,7 @@
             % plot localization precision in a separate figure window
             figure('Name', strcat(app.file, ' - Localization Precision')); % create new figure for the plots
             ax_x = subplot(app.nDim-1, 3, 1);
-            sigma_x = std_dist_x;
+            sigma_x = std_dist_x(std_dist_x~=0);
             median_sig_x = median(sigma_x);
             histogram(ax_x, sigma_x);
             line(ax_x, [median_sig_x, median_sig_x], ylim, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
@@ -47,7 +47,7 @@
             xlabel(ax_x, 'σ_{x}(nm)');
             
             ax_y = subplot(app.nDim-1, 3, 2);
-            sigma_y = std_dist_y;
+            sigma_y = std_dist_y(std_dist_y~=0);
             median_sig_y = median(sigma_y);
             histogram(ax_y, sigma_y);
             line(ax_y, [median_sig_y, median_sig_y], ylim, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
@@ -58,7 +58,7 @@
                 ax_xy = subplot(2, 3, 4);
                 
                 ax_z = subplot(2, 3, 3);
-                sigma_z = std_dist_z;
+                sigma_z = std_dist_z(std_dist_z~=0);
                 median_sig_z = median(sigma_z);
                 histogram(ax_z, sigma_z);
                 line(ax_z, [median_sig_z, median_sig_z], ylim, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
@@ -76,7 +76,7 @@
                 ax_xy = subplot(1, 3, 3);
             end
 
-            sigma_xy = std_dist_xy;
+            sigma_xy = std_dist_xy(std_dist_xy~=0);
             median_sig_xy = median(sigma_xy);
             histogram(ax_xy, sigma_xy);
             line(ax_xy, [median_sig_xy, median_sig_xy], ylim, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
